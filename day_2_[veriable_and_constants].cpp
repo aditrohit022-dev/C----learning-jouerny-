11076c82ada90a3ff0cb2be76ddeba3fdde51e1c
ADIT <aditrohit022@gmail.com>
2026-07-05 14:47:52 +0530

    day_2_[veriable_and_constants].cpp

---
+++ day_2_[veriable_and_constants].cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+
+using namespace std;
+int main() {
+    int score;
+        score=110;
+    int balence=500;
+        balence=1000;
+    const int unique_id = 4353; 
+    // unique_id=7783
+
+
+cout<< "welocome to my desktop with cpp "<<endl; 
+return 0 ;
+
+}
