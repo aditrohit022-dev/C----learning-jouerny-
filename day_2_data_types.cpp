77d8c7423aa5fba69d39e17188faa562024d9c05
ADIT <aditrohit022@gmail.com>
2026-07-05 17:03:27 +0530

    day_2_data_types.cpp

---
+++ day_2_data_types.cpp
@@ -0,0 +1,22 @@
+//data_types_cpp
+#include <iostream> 
+using namespace std;
+int main(){
+    int tealeves=50;
+    float  watertemp = 85.54678;
+    double  price = 10.56;
+    char grade ='A';
+    bool istea_ready=true;
+        // cout<<watertemp<<endl;
+
+
+return  0;
+}
+
+
+
+
+
+
+
+
---
+++ day_2_modifyer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+     unsigned int Tea_pack = 100; //with -ve int it will provide random garbage values
+    long large_tea_storege= 10000000000000000;
+    short mini_tea_pack=34;
+
+
+
+    // cout<<large_tea_storege;
+    return 0;
+}
