965a95d56931352dcc3cdaa96ee9c25ba03a915c
ADIT <aditrohit022@gmail.com>
2026-07-07 15:36:28 +0530

    Add tea information collection program
    This program collects and displays information about a specific type of tea, including its price and customer rating.

--- solution of challenge1.cpp
+++ solution of challenge1.cpp
@@ -1,1 +1,31 @@
 
+//CHALLENGE:1
+
+#include <iostream>
+#include <string>
+using namespace std; 
+int main(){
+    string Tea_type;
+    int price_kg;
+    int rating;
+
+        cout<<"Enter information about your tea\nEnter tea type: ";
+        getline(cin,Tea_type);
+        cout<<"Enter price of "<<Tea_type<<" tea per KG: ";
+        cin>>price_kg;
+        cout<<"Enter customer rating out of 10 in integer : ";
+        cin>>rating;
+        cout<<"-------------------------------------------------\n"
+            <<"Your tea information is succesfully added as under, "<<"\n"
+            <<"tea type: "<<Tea_type<<"\n"
+            <<"Price per kilogram: "<<price_kg<<"\n"
+            <<"Customer rating "<<rating<<"/10"<<"\n"; 
+    
+return 0;
+}   
+
+
+
+
+
+
