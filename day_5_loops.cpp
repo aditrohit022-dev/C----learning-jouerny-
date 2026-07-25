86b4fc2b4a8577dcf66fac81fee39b3de75d18ab
ADIT <aditrohit022@gmail.com>
2026-07-25 21:57:54 +0530

    Implement tea serving loop in day_5_loops.cpp
    Added a loop to serve tea cups based on user input.

---
+++ day_5_loops.cpp
@@ -0,0 +1,20 @@
+//#1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int tea;
+        cout<<"Enter numbers of tea cups to serve,"<<"Enter:";
+        cin<<tea;
+    tea-=1;
+    while(tea>0){
+        cout<<"serving a cup of tea \n"<<tea<<" cups of tea remaing";
+    }    
+    cout<<"all tea cups are served!";
+return 0;
+
+}
+
+
+
