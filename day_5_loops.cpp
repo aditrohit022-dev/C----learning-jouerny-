fc4354c733ccb72a0cf983108534c12f67c51dd4
ADIT <aditrohit022@gmail.com>
2026-07-25 23:42:30 +0530

    Implement tea brewing loop with skipping logic
    Added a loop to brew different types of tea, skipping 'Green Tea'.

--- day_5_loops.cpp
+++ day_5_loops.cpp
@@ -72,7 +72,27 @@ int main(){
 return 0;
 }
 
+//#5
 
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string tea[5]={"Orange Tea","Oolong Tea","Green Tea","Masala Tea","Cold Tea"};
+    
+    for(int i=0;i<5;i++){
+        if(tea[i]=="Green Tea"){
+            cout<<"Skipping the "<<tea[i]<<"...\n";
+            continue;
+        }
+        
+        cout<<"Brewing "<<tea[i]<<"...\n";
+        
+    }
+    cout<<"Outside the loop!";
+    
+return 0;
+}
 
 
 
