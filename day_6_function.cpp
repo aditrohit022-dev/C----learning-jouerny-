efd2a6d627f76c8536c572dcbb3df12f1f2eb3c2
ADIT <aditrohit022@gmail.com>
2026-07-26 16:31:37 +0530

    Implement function overloading for serveChai
    Added function overloading for serveChai with default value.

--- day_6_function.cpp
+++ day_6_function.cpp
@@ -26,8 +26,42 @@ void serveChai(int cups){
     
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+using namespace std;
 
 
+int checktemprature(int temprature){
+    return temprature;
+
+}
+
+
+//multi loading function// add defaulat value when declaring the function not in defining section 
+
+void serveChai(string teatype="Masala chai");
+void serveChai(int cups);
+int main() {
+	int temp=checktemprature(67);
+    //cout<<temp<<"\n";
+    //use of function 
+    serveChai();
+    
+}
+
+//defining function 
+
+void serveChai(string teatype){
+    cout<<"serving "<<teatype<<"...";
+}
+
+void serveChai(int cups){
+    
+    cout<<"serving "<<cups<<" cups of tea...";
+    
+}
 
 
 
