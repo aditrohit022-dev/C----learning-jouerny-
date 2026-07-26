448eabc71e1dab02233617ddd07154408c6f721d
ADIT <aditrohit022@gmail.com>
2026-07-26 15:35:00 +0530

    Refactor serveChai function and main output
    Updated the serveChai function to include output and uncommented code in main.

--- day_6_function.cpp
+++ day_6_function.cpp
@@ -1,17 +1,30 @@
-//defining fuction
-
 #include <iostream>
+using namespace std;
 
 int checktemprature(int temprature){
     return temprature;
-    
+
 }
+
+
+//declaration of function
+void serveChai(int cups);
+
 
 int main() {
 	int temp=checktemprature(67);
-	std::cout<<temp;
+    //cout<<temp<<"\n";
+    //use of function 
+    serveChai(8);
 }
 
+//defining function 
+
+void serveChai(int cups){
+    
+    cout<<"serving "<<cups<<" cups of tea...";
+    
+}
 
 
 
