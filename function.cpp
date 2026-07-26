1e1f775c622ce65c8ddf64f2b27b8f8166cf6354
ADIT <aditrohit022@gmail.com>
2026-07-26 15:23:15 +0530

    Add function to check temperature and display it

---
+++ function.cpp
@@ -0,0 +1,28 @@
+//defining fuction
+
+#include <iostream>
+
+int checktemprature(int temprature){
+    return temprature;
+    
+}
+
+int main() {
+	int temp=checktemprature(67);
+	std::cout<<temp;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
