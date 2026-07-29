e5cce4b9088bd480948864945646fe9f4e8dc995
ADIT <aditrohit022@gmail.com>
2026-07-29 19:52:27 +0530

    Add beginner project details for Simple Calculator
    Added project description for a beginner-level calculator.

--- PROJECTS_ON_SWITCH_EASY TO HARD.cpp
+++ PROJECTS_ON_SWITCH_EASY TO HARD.cpp
@@ -1,1 +1,28 @@
 
+//🟢 Level 1: Beginner (Getting the Basics Down)
+//These projects focus strictly on syntax, handling case labels, break statements, and the default case.
+
+//1. Simple Calculator
+//The Goal: Ask the user to enter two numbers and an operator (+, -, *, /).
+
+//Switch Focus: Switch on the operator character (char).
+
+//Key Concept: Practice standard arithmetic and use the default case to catch invalid operators (like % or @). 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
